3627f608536090293555aaabe7dfc49a8ad9b9be
Julien Bordellier <git@julienbordellier.com>
2014-07-28 12:22:23 +0200

    Changing the architecture of all for the 0.0.1

--- lib/docker_pilot/docker_pilot_object.rb
+++ lib/docker_pilot/docker_pilot_object.rb
@@ -1,4 +1,13 @@
+require 'active_support/core_ext'
+
 module DockerPilot
   class DockerPilotObject
+    def initialize(values={})
+      @values = values
+    end
+
+    def update_values(values)
+      @values.reverse_merge(values)
+    end
   end
 end
